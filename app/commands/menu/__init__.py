ebde068b90af2b1cae78fb10e7aafd33f297852a
gvr1220 <gv225@njit.edu>
2024-02-29 12:18:05 -0500

    update

---
+++ app/commands/menu/__init__.py
@@ -0,0 +1,11 @@
+import sys
+from app.commands import Command
+
+
+class MenuCommand(Command):
+    def execute(self):
+        print("Displaying menu:")
+        print("1. add")
+        print("2. subtract")
+        print("3. multiply")
+        print("3. divide")
